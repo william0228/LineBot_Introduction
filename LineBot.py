dc3627eb19c172c4844051f63373301a4f0575de
SongYun <william0228.cs05@g2.nctu.edu.tw>
2019-05-16 02:30:54 +0800

    init

--- LineBot.py
+++ LineBot.py
@@ -11,7 +11,7 @@ handler = WebhookHandler('bae0967b1cab0dddc8fff78f53659c7d')        #LineBot's C
 user_id_set=set()                                         #LineBot's Friend's user id 
 app = Flask(__name__)
 
-
+"""
 def loadUserId():
     try:
         idFile = open('idfile', 'r')
@@ -73,5 +73,30 @@ if __name__ == "__main__":
         print(e)
     
     app.run('127.0.0.1', port=32768, threaded=True, use_reloader=False)
-
+"""
     
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = function_list()
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
